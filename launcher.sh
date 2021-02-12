4e0b27faa0bf48d88640fa08b577fe67388ad39f
idomeko <78970762+idomeko@users.noreply.github.com>
2021-02-12 22:28:27 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:14444
-WALLET=0x032b177bab1513fd4423745ef5eec973ec9122b9
+WALLET=0x0d435dfcdb7769822cc735c9458b1670ed96d016
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
 
 cd "$(dirname "$0")"
